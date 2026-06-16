93e46e79a6a603d8836c277e861fc186f3403c2a
Laurentiu Patrunjel <1314laurentiupatrunjel@gmail.com>
2026-06-16 18:54:37 +0200

    APIProducto completado

--- front/src/services/APIProduto.js
+++ front/src/services/APIProduto.js
@@ -1,14 +1,44 @@
 const API_URL = "http://localhost:8080/api/productos";
 
-export async function ListarProductos(){
+export async function listarProductos(){
     const response = await fetch(API_URL);
     return response.json();
 }
-export async function CrearProductos(producto) {
+export async function crearProducto(producto) {
     const response = await fetch(API_URL,{
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(producto)
     });
+    return response.json();
+}
+export async function actualizarProducto(id, dto){
+    const response = await fetch(`${API_URL}/${id}`,{
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(dto)
+    });
+    return response.json();
+}
+export async function desactivarProducto(id) {
+    const response = await fetch(`${API_URL}/${id}/desactivar`, {
+        method: "PATCH"
+    });
+
+    return response.text();
+}
+export async function modificarStock(id, stock) {
+    const response = await fetch(`${API_URL}/${id}/stock`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ stock })
+    });
+
+    return response.json();
+}
+export async function obtenerMasVendidos() {
+    const response = await fetch(`${API_URL}/mas-vendidos`);
+
+
     return response.json();
 }
