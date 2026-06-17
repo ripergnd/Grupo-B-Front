ef85c4213eefe5ee05f9295ba6a903e90db36050
jacerc <javiercerveracentenero@gmail.com>
2026-06-17 18:46:12 +0200

    added API obtenerPedidoPorCodigo

--- front/src/services/APIPedido.js
+++ front/src/services/APIPedido.js
@@ -1,57 +1,62 @@
 const API_URL = "http://localhost:8080/api/pedidos";
 
 export async function listarPedidos() {
-    const response = await fetch(API_URL);
-    return response.json();
+  const response = await fetch(API_URL);
+  return response.json();
 }
 
 export async function obtenerPedido(id) {
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+export async function obtenerPedidoPorCodigo(codigo) {
+  const response = await fetch(`${API_URL}/codigo/${codigo}`);
+  return response.json();
 }
 
 export async function crearPedido(dto) {
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(dto)
-    });
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dto),
+  });
 
-    return response.json();
+  return response.json();
 }
 
 export async function agregarProducto(pedidoId, productos) {
-    const response = await fetch(`${API_URL}/${pedidoId}/productos`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(productos)
-    });
+  const response = await fetch(`${API_URL}/${pedidoId}/productos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productos),
+  });
 
-    return response.json();
+  return response.json();
 }
 
 export async function eliminarProducto(pedidoId, productoId) {
-    const response = await fetch(
-        `${API_URL}/${pedidoId}/productos/${productoId}`,
-        {
-            method: "DELETE"
-        }
-    );
+  const response = await fetch(
+    `${API_URL}/${pedidoId}/productos/${productoId}`,
+    {
+      method: "DELETE",
+    },
+  );
 
-    return response.json();
+  return response.json();
 }
 
 export async function cambiarEstado(pedidoId, estado) {
-    const response = await fetch(
-        `${API_URL}/${pedidoId}/estado?estado=${estado}`,
-        {
-            method: "PATCH"
-        }
-    );
+  const response = await fetch(
+    `${API_URL}/${pedidoId}/estado?estado=${estado}`,
+    {
+      method: "PATCH",
+    },
+  );
 
-    return response.json();
+  return response.json();
 }
